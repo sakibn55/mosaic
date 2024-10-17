4b8c1c71667e5690e5d6f65e39a69218092d2dc8
Nazmus Sakib <sakibn55@gmail.com>
2024-10-17 14:52:28 -0400

    vuejs config

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import { createApp } from "vue";
+import ExampleComponent from "./components/ExampleComponent.vue";
+
+createApp({
+    components: {
+        ExampleComponent,
+    },
+}).mount("#app");
